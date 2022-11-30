2986a385861b19e1c2d39ec8e9d3aa0dc1f1baa2
Irina Shorina <aprams@inbox.ru>
2022-11-30 21:44:17 +0300

    Ирина класс заявок

--- carwash/washapp/models.py
+++ carwash/washapp/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-# иРИНА пишет
+
+class Zayavki(models.Model):
+    content=models.TextField()
+    clientsid = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    date=models.DateField()
+    time=models.TextField()
+
+
+
