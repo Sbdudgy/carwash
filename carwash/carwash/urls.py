82aafd889470fc1df1504018fe7170c2d8acccdc
Irina Shorina <aprams@inbox.ru>
2022-11-30 22:15:40 +0300

    Ирина Шорина

--- carwash/carwash/urls.py
+++ carwash/carwash/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from washapp.views import washappView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('washapp/', washappView),
 ]
--- carwash/washapp/views.py
+++ carwash/washapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-
+def washappView(request):
+    return render(request,'washlist.html')
 
