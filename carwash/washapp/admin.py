d5a741c754d7a2e6376fe35f33af37b5414a7d98
Viktoriia <sbdudgy@mail.ru>
2022-11-30 22:17:14 +0300

    commit db to admin

--- carwash/washapp/admin.py
+++ carwash/washapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Zayavki, Client, Timetable
+
+admin.site.register(Zayavki)
+admin.site.register(Client)
+admin.site.register(Timetable)
+
 # Register your models here.
