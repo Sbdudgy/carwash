982b580c99800f924b3846250e8544d3a43d73be
Viktoriia <sbdudgy@mail.ru>
2022-11-30 21:49:18 +0300

    db change

--- carwash/washapp/models.py
+++ carwash/washapp/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Client(models.Model):
+    name=models.TextField()
+    number=models.IntegerField()
+
 class Zayavki(models.Model):
-    content=models.TextField()
-    clientsid = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    clientsid = models.ForeignKey(Client, on_delete=models.CASCADE)
     date=models.DateField()
     time=models.TextField()
 
-
+class Timetable(models.Model):
+    time=models.TextField()
 
