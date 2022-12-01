3f25ffb709933c327b8d17eec329fa7de36169a7
Irina Shorina <aprams@inbox.ru>
2022-12-01 17:22:57 +0300

    update

--- carwash/carwash/urls.py
+++ carwash/carwash/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from washapp.views import washappView
+from washapp.views import washappView,getTimeFreeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('washapp/', washappView),
+    path('TimeFree/', getTimeFreeView),
 ]
--- carwash/washapp/views.py
+++ carwash/washapp/views.py
@@ -4,5 +4,20 @@ from .models import Zayavki, Timetable
 def washappView(request):
     vse_zayav=Zayavki.objects.all()
     vse_time= Timetable.objects.all()
+    #req=request.POST['datewash']
+    #for i in vse_zayav:
+        #if i.date==req:
+            #vse_time.filter(id=i.id).delete()
     return render(request,'washlist.html', {'all_zayav':vse_zayav,'all_time':vse_time})
+
+def getTimeFreeView(request):
+    vse_zayav=Zayavki.objects.all()
+    vse_time= Timetable.objects.all()
+    req=request.POST['datewash']
+    for i in vse_zayav:
+        if i.date==req:
+            vse_time.filter(id=i.id).delete()
+    return render(request,'timefree.html', {'all_zayav':vse_zayav,'all_time':vse_time})
+    return HttpResponseRedirect('/todoapp')
+    return render(request,'todolist.html',{'i':new_item})
 
