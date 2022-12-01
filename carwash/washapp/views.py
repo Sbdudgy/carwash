2709af5013a94110327543ede03e097591993381
Viktoriia <sbdudgy@mail.ru>
2022-12-01 16:32:52 +0300

    html

--- carwash/washapp/views.py
+++ carwash/washapp/views.py
@@ -4,5 +4,5 @@ from .models import Zayavki, Timetable
 def washappView(request):
     vse_zayav=Zayavki.objects.all()
     vse_time= Timetable.objects.all()
-    return render(request,'washlist.html', {'all_zayav':vse_zayav},{'all_time':vse_time})
+    return render(request,'washlist.html', {'all_zayav':vse_zayav,'all_time':vse_time})
 
