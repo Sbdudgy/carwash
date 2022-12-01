8a1e6e84efc3c9f4848dbcb5ad8e52a0f185df76
Irina Shorina <aprams@inbox.ru>
2022-12-01 17:46:40 +0300

    updateIr

--- carwash/washapp/views.py
+++ carwash/washapp/views.py
@@ -4,20 +4,21 @@ from .models import Zayavki, Timetable
 def washappView(request):
     vse_zayav=Zayavki.objects.all()
     vse_time= Timetable.objects.all()
-    #req=request.POST['datewash']
-    #for i in vse_zayav:
-        #if i.date==req:
-            #vse_time.filter(id=i.id).delete()
     return render(request,'washlist.html', {'all_zayav':vse_zayav,'all_time':vse_time})
 
 def getTimeFreeView(request):
     vse_zayav=Zayavki.objects.all()
     vse_time= Timetable.objects.all()
+    vse_nuzh=[]
+    for k in vse_time:
+        vse_nuzh.append(k.time)
     req=request.POST['datewash']
     for i in vse_zayav:
         if str(i.date)==str(req):
-            vse_time.filter(time=i.time).delete()
-    return render(request,'timefree.html', {'all_zayav':vse_zayav,'all_time':vse_time,'req':req})
-    return HttpResponseRedirect('/todoapp')
-    return render(request,'todolist.html',{'i':new_item})
+            #vse_time.filter(time=i.time).delete()
+            vse_nuzh.remove(i.time)
+        #else:
+            #vse_time= Timetable.objects.all()
+    return render(request,'timefree.html', {'all_zayav':vse_zayav,'all_time':vse_time,'req':req,'vse_nuzh':vse_nuzh})
+    
 
