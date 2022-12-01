29b3f7f3271cef8598f01cd60e071f8683c1fb06
Irina Shorina <aprams@inbox.ru>
2022-12-01 16:23:14 +0300

    Ирина

--- carwash/washapp/views.py
+++ carwash/washapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Zayavki, Timetable
 
 def washappView(request):
-    return render(request,'washlist.html')
+    vse_zayav=Zayavki.objects.all()
+    vse_time= Timetable.objects.all()
+    return render(request,'washlist.html', {'all_zayav':vse_zayav},{'all_time':vse_time})
 
