f88d28587910c7027940de074cb08e22f7fd3f9b
Viktoriia <sbdudgy@mail.ru>
2022-12-06 23:43:03 +0300

    sett

--- carwash/carwash/settings.py
+++ carwash/carwash/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-a$=0c2abxr=(wsm=s#imhi84zo3p89m)qd8r7l&pkd$l2l$-hy"
+SECRET_KEY = "django-insecure-a$=0c2abxr=(wsm=s#imhi84zo3p89m)qd8r7l&pkd$l2l$-hy"
 
-SECRET_KEY =os.environ['SECRET_KEY']
+# SECRET_KEY =os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
